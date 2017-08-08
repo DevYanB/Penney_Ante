da86dc04f4e662050ecd879b2622dbc9b3ddf538
Daeka24 <devyanbiswas@gmail.com>
2017-08-08 00:25:37 -0700

    Update Penney_Ante_Code.py

--- Penney_Ante_Code.py
+++ Penney_Ante_Code.py
@@ -1,30 +1,40 @@
 import time
 import random
 
-#Perhaps have it analyze player strategy? Or make its decisions more random
+#This is a code inspired by the Penney Ante Logic and Chance Game! Time.sleep's are included for dramatic effect!
 usr_score = 0
 cmp_score = 0
 
-#First step: very basic. Asks user for their input, uses the basic strategy for highest probability of winning to choose its own
-#Initiates the game
+#First section asks user for their input, uses the basic strategy for highest probability of winning to choose its own;
+#Based on non-transitive probability tree of this game(check online), if user gives ABC, optimal result is (!B)AB;
+#Makes sure string input is valid;
+#Initiates the game.
 print("Hello! Welcome to the Penney Ante Game! \nPick a series of three coin-flip results!\nFirst to 5 wins! \n*Please use lowercase!*")
-usr = input()
+while True:
+    usr = input()
+    if((len(usr) < 3) or (len(usr) > 3)):
+       print("Enter a valid sequence")
+       continue;
+    else:
+       break
+
 mid = usr[1:2]
 if mid == 't':
     cmp = 'h' + usr[0:2]
 elif mid == 'h':
     cmp = 't' + usr[0:2]
 print("Alright, good choice!")
+
 time.sleep (2)
 print("I choose: " + cmp)
 time.sleep(2)
 print("Now, let us play!")
 print("")
-time.sleep(5)
+time.sleep(3)
 
-#This next portion is the actual game
-#It randomizes the head or tail values corresponding to 1 or 2 and adds them to a list l
-#Then, it checks if a sequence of three appears mathcing either the user's or computer's choice
+#This next portion is the actual game;
+#It randomizes the head or tail values corresponding to 1 or 2 and adds them to a list l;
+#Then, it checks if a sequence of three appears mathcing either the user's or computer's choice;
 #If it does, it will add a point to the respective party and start flipping again!
 
 l = []
@@ -38,11 +48,13 @@ while(c <= 10000):
     else:
         l.append('t')
     print(l)
-    time.sleep(2)
-    a = len(l)
-    if a >= 3:
-        n = c+3
-        v = ''.join(l[(n-6):n])
+    time.sleep(1)
+    if len(l) >= 3:
+        n = c-3
+        v = ''.join(l[n:(n+3)]) #This function here creates a string of the last 3 terms(once 3 or more terms long) from the array l
+                                #and adds spaces between them for comparison to the user inputted string;
+                                #Can also just use basic string and concatenation without spaces, but important fact is that it uses 3 most recent terms;
+                                #Subtracting 3 from c, setting it as n, then getting substring from n to n+3. works well i think.
         if v == usr:
             usr_score += 1
             l.clear()
